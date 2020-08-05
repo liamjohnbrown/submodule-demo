236646733b078955ea0f6f9fea657cc80e697461
Liam <liam.brown@education.gov.uk>
2020-08-05 15:49:16 +0100

    Create use_two_inputs()
    use_two_inputs(a, b) will be used to demonstrate the use of git submodules. At present it finds the sum of the inputs a and b, but it will be changed in later commits to perform different operations using two inputs (hence the vague name).

---
+++ R/use_two_inputs.R
@@ -0,0 +1,16 @@
+# This file describes a single function which is changed in different commits 
+# for to a demonstration of the use of git submodules.
+
+#' Add together two numbers.
+#' 
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' use_two_inputs(1, 1)
+#' use_two_inputs(10, 1)
+use_two_inputs <- function(a, b){
+  
+  a + b
+  
+}
