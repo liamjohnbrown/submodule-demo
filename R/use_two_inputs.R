14b782d798fbc81d1d71003b12ba08e6aabec5f2
Liam <liam.brown@education.gov.uk>
2020-08-05 16:07:37 +0100

    Change use_two_inputs() to subtract, not add, inputs
    Hence from this commit onward use_two_inputs() will behave differently to previous commits. The intention is that the demonstration of git submodules will point to two commits, where use_two_inputs() behaves differently in each.

--- R/use_two_inputs.R
+++ R/use_two_inputs.R
@@ -1,16 +1,16 @@
 # This file describes a single function which is changed in different commits 
-# for to a demonstration of the use of git submodules.
+# for a demonstration of the use of git submodules.
 
-#' Add together two numbers.
+#' subtract one number from another.
 #' 
 #' @param x A number.
 #' @param y A number.
-#' @return The sum of \code{x} and \code{y}.
+#' @return The difference of \code{x} and \code{y}.
 #' @examples
-#' use_two_inputs(1, 1)
-#' use_two_inputs(10, 1)
-use_two_inputs <- function(a, b){
+#' use_two_inputs(3, 1)
+#' use_two_inputs(1, 3)
+use_two_inputs <- function(x, y){
   
-  a + b
+  x - y
   
 }
